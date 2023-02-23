626532ed0f2dbe83ff2e46661c894b39090118a7
ericjonesdev <ericjones2004@yahoo.com>
2023-02-23 20:20:40 +0000

    Fix error in installed apps part 2

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'cloudinary_storage'
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'blog',
