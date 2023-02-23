b0982cfcf602c16f2c77dee1d8967871754e98e0
ericjonesdev <ericjones2004@yahoo.com>
2023-02-23 19:29:31 +0000

    Correct error in line 103 regarding URL in settings.py

--- codestar2021/settings.py
+++ codestar2021/settings.py
@@ -90,17 +90,17 @@ WSGI_APPLICATION = 'codestar2021.wsgi.application'
 
 DEBUG = 'DEVELOPMENT' in os.environ
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
-    DATABASES = {
-        'default':
-        dj_database_url.parse(os.environ.get("DATABASE_url"))
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+# else:
+DATABASES = {
+    'default':
+    dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
